485840805ce99107a22df86f94339b92da15843e
Ferchus-24 <fernanda.prestinoni@fce.uncu.edu.ar>
2023-11-20 19:57:34 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ function buscarLibro() {
 function calcularTarifa() {
     let tarifaBase = 50
     let descuento = 0
-    let edad= prompt("Cual es su edad?")
-    let duracionMembresia = prompt("Cuanto quiere que dure la membresia")
+    let edad= parseInt(prompt("¿Cuál es su edad?"))
+    let duracionMembresia = parseInt(prompt("¿Cuántos años quiere que dure la membresia?"))
     if (edad < 18) {
         descuento = 20
     } else if (edad >= 65) {
@@ -55,7 +55,7 @@ function calcularTarifa() {
 
     let tarifaTotal = tarifaBase - (tarifaBase * (descuento / 100));
     let tarifaMembresia = tarifaTotal * duracionMembresia;
-    alert("Su tarifa de membresía sería de $ "+ tarifaMembresia)
+    alert("Su tarifa de membresía será de $"+ tarifaMembresia)
 }
 
 
@@ -63,3 +63,59 @@ validarUsuario()
 buscarLibro()
 calcularTarifa()
 
+const Libro = function(nombre,anio,apellidoAutor, stock){       
+    this.nombre = nombre
+    this.anio = anio
+    this.apellidoAutor = apellidoAutor
+    this.stock = stock
+}
+
+let libro1 = new Libro("El aleph", 1945, Borges, 5)
+let libro2 = new Libro("Ficciones", 1944, Borges, 3)
+let libro3 = new Libro("La tregua", 1960, Benedetti, 6)
+let libro4 = new Libro("Gracias por el fuego", 1966, Benedetti, 2)
+let libro5 = new Libro("Rayuela", 1963, Cortazar, 4)
+let libro6 = new Libro("Mujeres", 2015, Galeano, 1)
+
+
+
+
+let lista = [libro1,libro2,libro3,libro4,libro5,libro6]
+
+
+function filtrarLibros(){
+let palabraClave = prompt("ingresa el libro que estas buscando").toUpperCase().trim()
+let resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(palabraClave))
+
+
+if(resultado.length >0){
+    console.table(resultado)
+}else{
+    alert("no se encontro el libro " + palabraClave)
+    let respuesta= confirm("lo Quiere agregar un nuevo libro?")
+
+    if(respuesta == true ){
+        agregarLibro()
+    }
+}
+}
+
+
+function agregarLibro(){
+
+let nombre = prompt("ingresa el nombre del libro")
+let anio = parseInt(prompt("ingresa el año de edición del libro")) 
+let apellidoAutor = prompt("ingresa el nombre del libro")
+let stock = parseInt(prompt("ingresa el stock del libro"))
+
+
+if(isNaN(anio) || isNaN(stock) || nombre==="" || apellidoAutor===""){
+    alert("por favor ingresa valores validos")
+    return
+}
+
+let libro = new Libro(nombre,anio,apellidoAutor,stock)
+
+lista.push(libro)
+console.table(lista)
+}
